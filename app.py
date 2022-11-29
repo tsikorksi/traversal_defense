e45f920e15b50c86b0e75a552786975e033f9257
tsikorksi <tsikors2@jh.edu>
2022-11-29 16:53:10 -0500

    added anti-brute

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import random
+import string
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +9,22 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
 	return 'Hello World!'
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+	"""
+	Anti-Brute: We write a catch-all function that generates a page of random size,
+	thus preventing tools like dirb
+	:param path:
+	:return:
+	"""
+	return render_template("random_200.html", random_text=random_string(200))
+
+
+def random_string(n):
+	return str(''.join(random.choices(string.ascii_uppercase + string.digits, k=n)))
 
 
 if __name__ == '__main__':
